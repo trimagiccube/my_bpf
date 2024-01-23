2d37665db7e4a356790b41b819bebe045ce5e70e
xiaowen.yan <xiaowen.yan@iluvatar.ai>
2024-01-23 09:30:04 +0000

    Initial commit

---
+++ bpf_sample/hello_user.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <linux/bpf.h>
+#include "bpf_load.h"
+#include "libbpf.h"
+
+int main(int argc, char **argv)
+{
+	if( load_bpf_file("hello_kern.o") != 0)
+	{
+		printf("The kernel didn't load BPF program\n");
+		return -1;
+	}
+
+	read_trace_pipe();
+	return 0;
+}
